857ea61517e991fe42907c12452d5e1aff985ba7
Vinula Senarathne <vinulapc@gmail.com>
2024-10-23 14:25:37 +0530

    feat: chat search field

--- src/components/AllChats.jsx
+++ src/components/AllChats.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { ReactComponent as SearchIcon } from "./../assets/icons/search.svg";
 
 const AllChats = () => {
-  return <div className="text-black">AllChats</div>;
+  return (
+    <div className="flex">
+      {/* Chat inbox */}
+      <div className="flex w-full flex-col">
+        {/* Search input */}
+        <div className="flex items-center relative max-w-[300px]">
+          <SearchIcon className="w-5 h-5 absolute ml-3" />
+          <input
+            type="text"
+            placeholder="Search"
+            className="border-none w-full outline-none pl-10 py-3 bg-[#DBDCFF] p-2 rounded-xl text-xs placeholder:text-[#202022]"
+          />
+        </div>
+        {/* Chat inbox cards */}
+        <div></div>
+      </div>
+
+      {/* Chat messages */}
+      <div></div>
+    </div>
+  );
 };
 
 export default AllChats;
