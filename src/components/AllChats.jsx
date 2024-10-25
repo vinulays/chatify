eae895a14cda1a7c091f17d8539889e49fbd40e9
Vinula Senarathne <vinulapc@gmail.com>
2024-10-25 15:02:42 +0530

    feat: chat header and input field

--- src/components/AllChats.jsx
+++ src/components/AllChats.jsx
@@ -1,15 +1,23 @@
 import React from "react";
-import { ReactComponent as SearchIcon } from "./../assets/icons/search.svg";
+import {
+  PaperAirplaneIcon,
+  MicrophoneIcon,
+  MagnifyingGlassIcon,
+  PhoneIcon,
+  EllipsisVerticalIcon,
+} from "@heroicons/react/24/outline";
+import { PaperClipIcon } from "@heroicons/react/24/solid";
+
 import InboxCard from "./InboxCard";
 
 const AllChats = () => {
   return (
-    <div className="flex h-full">
+    <div className="flex  h-full">
       {/* Chat inbox */}
-      <div className="flex flex-col w-full">
+      <div className="flex flex-col">
         {/* Search input */}
-        <div className="flex items-center relative max-w-[320px] px-3">
-          <SearchIcon className="w-5 h-5 absolute ml-3 fill-current text-gray-600" />
+        <div className="flex items-center relative px-3">
+          <MagnifyingGlassIcon className="w-5 h-5 absolute ml-3 text-gray-600" />
           <input
             type="text"
             placeholder="Search"
@@ -17,7 +25,7 @@ const AllChats = () => {
           />
         </div>
         {/* Chat inbox cards */}
-        <div className="flex flex-col max-w-[320px] mt-3 scroll">
+        <div className="flex flex-col mt-3 scroll">
           <InboxCard />
           <InboxCard />
           <InboxCard />
@@ -31,7 +39,38 @@ const AllChats = () => {
       </div>
 
       {/* Chat messages */}
-      <div></div>
+      <div className="flex flex-1 flex-col px-10 justify-between">
+        <div>
+          <div className="flex items-center justify-between">
+            <div className="flex flex-col gap-1">
+              <div className="text-2xl font-medium">Georgiana Aurax</div>
+              <div className="text-sm text-gray-400">
+                Last online at 7.30 a.m
+              </div>
+            </div>
+            <div className="flex gap-6">
+              <MagnifyingGlassIcon className="w-6 h-6 text-gray-600 cursor-pointer" />
+              <PhoneIcon className="w-6 h-6  text-gray-600 cursor-pointer" />
+              <EllipsisVerticalIcon className="w-6 h-6 text-gray-600 cursor-pointer" />
+            </div>
+          </div>
+        </div>
+        <div className="w-full">
+          <div className="flex items-center relative">
+            <PaperClipIcon className="w-5 h-5 absolute ml-3 fill-current text-gray-600 cursor-pointer" />
+
+            <input
+              type="text"
+              placeholder="Your message"
+              className="border-none w-full outline-none pl-11 pr-24 py-4 bg-[#EEEEF8] p-2 rounded-xl text-xs"
+            />
+            <div className="flex items-center gap-1 absolute right-6">
+              <MicrophoneIcon className="w-5 h-5 ml-3  text-gray-600 cursor-pointer" />
+              <PaperAirplaneIcon className="w-5 h-5 ml-3 text-gray-600 cursor-pointer" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
