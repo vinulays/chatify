ce8075adb9964654c3174767e7b84ae1dd290029
Vinula Senarathne <vinulapc@gmail.com>
2024-11-01 18:29:33 +0530

    feat: error toasts for duplicate usernames & email

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -18,6 +18,16 @@ exports.signup = async (req, res) => {
       .status(201)
       .json({ message: "User created successfully", username: user.username });
   } catch (error) {
+    if (error.code === 11000) {
+      const duplicateField = Object.keys(error.keyValue)[0];
+      const message =
+        duplicateField === "username"
+          ? "Username already exists. Please sign in."
+          : "Email address already exists. Please sign in.";
+      return res.status(400).json({ message });
+    }
+
+    // General error handler
     res.status(500).json({ message: "User creation failed", error });
   }
 };
